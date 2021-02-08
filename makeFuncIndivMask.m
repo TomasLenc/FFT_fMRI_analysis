d0f45c60e9f9dc00291e8518c0ac17c3b7657a0e
Ceren <battal.ceren@gmail.com>
2021-02-08 16:43:48 +0100

    cleaning from betImage

--- makeFuncIndivMask.m
+++ makeFuncIndivMask.m
@@ -2,19 +2,13 @@
 
 function mask = makeFuncIndivMask(opt)
 
-  % function uses a mean functional image to create individual space mask
-
-  % STEP 1
-  % go to mricron and create skull stripped mean functional image
-  % by using FSL BET function
-  % in the future think about implementing FSL BET into matlab
+  % function cpp-spm repo/functions and create skull stripped 
+  % mean functional image
 
   % read the mask image
   image = opt.funcMaskFileName;
   [imagePath, imageName, ext] = fileparts(image);
 
-  % the output name for FSL BET image (skull stripped)
-  % betImageName = ['bet05_', imageName, ext];
 
   %SPM skull stripping - with Anat atm
   [~, opt, BIDS] = getData(opt);
@@ -22,7 +16,9 @@ function mask = makeFuncIndivMask(opt)
   
   opt.orderBatches.segment = 1;
   opt.orderBatches.skullStripping = 2;
-  opt.skullStripMeanImg = 1; % skull strip the mean image
+  
+  % skull strip the mean image
+  opt.skullStripMeanImg = 1; 
   
   % make matlab batch for segment and skullstip
   matlabbatch = [];
@@ -33,44 +29,11 @@ function mask = makeFuncIndivMask(opt)
   saveAndRunWorkflow(matlabbatch, 'meanImage_segment_skullstrip', opt, subID);
 
 
-  % STEP 1.2
-  % read already created bet05 image
-
- % betImage = fullfile(imagePath, betImageName);
-
-%   % STEP 1.3
-%   % copy created bet05_meanfunc_mask images & rename with taskname
-%   if ~exist(betImage)
-% 
-%     rhythmBlockFuncDir = strrep(imagePath, opt.taskName, 'RhythmBlock');
-%     betOrigImage = strrep(fullfile(rhythmBlockFuncDir, betImageName), ...
-%                           opt.taskName, 'RhythmBlock');
-%     copyfile(betOrigImage, betImage);
-% 
-%   end
-
-  % create mask name
+  % output the mask
   [meanImage, meanFuncDir] = getMeanFuncFilename(BIDS, subID, opt);
   %name the output accordingto the input image
   maskFileName = ['m' strrep(meanImage, '.nii', '_mask.nii')];
   mask = fullfile(meanFuncDir, maskFileName);
-% 
-%   % STEP 2
-%   if ~exist(mask)
-%     % Create a template & load the mask
-%     % A = load_untouch_nii('bet_05_meanuasub-pil001-PitchFT_run-001.nii');
-%     A = load_untouch_nii(maskFileName);
-% 
-%     C = A;
-%     C.fileprefix = 'C';
-%     C.img = [];
-% 
-%     idx = find(A.img > 0);
-%     A.img(idx) = 1;
-%     C.img = A.img;
-%     save_untouch_nii(C, mask);
-%   end
-% 
-% end
+
 
 
