27ffb20c83f2e72132ce692febcf677c85596797
Ceren <battal.ceren@gmail.com>
2021-11-18 22:04:29 +0100

    minor fix + indent

--- groupLevelzMapThreshold.m
+++ groupLevelzMapThreshold.m
@@ -9,32 +9,38 @@ subNb = length(opt.subjects);
 
 %cut off threshold for pvalue
 threshold =  round(abs(norminv(pvalue)),2);
-        
+
+% prepare pvalue for labeling the output
+pvalue = strrep(num2str(pvalue), '.', '');
+
 % dummy call
 subLabel = opt.subjects{1};
 
-        % setup output directory
-        fftDir = getFFTdir(opt, subLabel);
-        [~, folder] = fileparts(fftDir);
-        inputFolder = fullfile(fftDir, '..', 'group', folder);
-        
-        fftFileName = getZimage(inputFolder, subNb);
+% setup output directory
+fftDir = getFFTdir(opt, subLabel);
+[~, folder] = fileparts(fftDir);
+inputFolder = fullfile(fftDir, '..', '..', '..','group', folder);
 
-        % load image
-        zHdr = spm_vol(fftFileName);
-        zImg = spm_read_vols(zHdr);
-        
-        % threshold image with z-conversion of p<1e-4 
-        zImg = zImg > threshold;
-        
-        % if option.save zmap correct save thresholded map
-         % save zscore image
-        if opt.save.zmap
-            newName = [pvalue, fftFileName];
-           saveThresholdedImage(zImg, zHdr, newName);
-        end
-        
-        
+fftFile = getZimage(inputFolder, subNb);
+
+% load image
+zHdr = spm_vol(fftFile);
+zImg = spm_read_vols(zHdr);
+
+% threshold image with z-conversion of p<1e-4
+% zImg = zImg > threshold; % this is binary conversion
+idx = zImg < threshold;
+zImg(idx) = 0;
+
+% if option.save zmap correct save thresholded map
+% save zscore image
+if opt.save.zmap
+    [~, name , ~] = fileparts(fftFile);
+    newName = [name, '_thres-p', pvalue];
+    saveThresholdedImage(zImg, zHdr, newName);
+end
+
+
 end
 
 
@@ -42,7 +48,7 @@ end
 function outputImage = getZimage(fftDir, subNb)
 % find the zmap in a given folder
 
-pattern = ['^whole-brain_AvgZTarget_subNb-', numb2str(subNb),'.nii$'];
+pattern = ['^whole-brain_AvgZTarget_subNb-', num2str(subNb),'.nii$'];
 outputImage = spm_select('FPList', fftDir, pattern);
 
 
